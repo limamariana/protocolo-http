fa9fe6ec875e6807910028532297b5b798ad043a
marianaRodLima <marianarlima93@gmail.com>
2021-06-28 14:59:45 -0300

    Projeto simples em C# utulizando Protocolo HTTP

---
+++ HttpProj/HttpProj/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace HttpProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReqList();
+            ReqUnica();
+            Console.ReadLine();
+
+
+        }    
+            static void ReqList()
+            {
+                var requisition = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+                requisition.Method = "GET";
+                var response = requisition.GetResponse();
+                using (response)
+                {
+                    var stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+                    object data = reader.ReadToEnd();
+
+                    //Console.WriteLine(data.ToString());
+
+                    List<Task> tasks = JsonConvert.DeserializeObject<List<Task>>(data.ToString());
+
+                    foreach (Task task in tasks)
+                    {
+                        task.Show();
+                    }
+
+                    stream.Close();
+                    response.Close();
+                }
+            }
+            
+            static void ReqUnica()
+            {
+                var requisition = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/5");
+                requisition.Method = "GET";
+                var response = requisition.GetResponse();
+                 using (response)
+                 {
+                    var stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream);
+                    object data = reader.ReadToEnd();
+
+                    Task task = JsonConvert.DeserializeObject<Task>(data.ToString());
+                    task.Show();
+                    
+                    stream.Close();
+                    response.Close();
+                 }
+            }
+    }
+}
---
+++ HttpProj/HttpProj/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpProj
+{
+    class Task
+    {
+
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Show()
+        {
+            Console.WriteLine("Task Object");
+            Console.WriteLine($"User Id: {userId}");
+            Console.WriteLine($"Id: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Completed: {completed}");
+            Console.WriteLine(" ");
+            Console.WriteLine("====================");
+
+
+        }
+    
+    }
+}
